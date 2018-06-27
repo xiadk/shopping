9590033062bcb3d4739fa53b9fbbe3e9163f580b
xiadk <13047901299@139.com>
2018-06-28 01:22:11 +0800

    提交

---
+++ src/main/java/com/dk/zb/ZbApplication.java
@@ -0,0 +1,23 @@
+package com.dk.zb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+//@SpringBootApplication
+public class ZbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbApplication.class, args);
+	}
+
+	@RequestMapping("/index")
+	public static String test(){
+
+	    return "hello world";
+    }
+}
