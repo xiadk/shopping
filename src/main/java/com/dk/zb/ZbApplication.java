d1071069e8749d5d2b02b55cfb86ce865d1cc78f
xiadk <13047901299@139.com>
2018-06-29 00:53:23 +0800

    更新

--- src/main/java/com/dk/zb/ZbApplication.java
+++ src/main/java/com/dk/zb/ZbApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-//@SpringBootApplication
+//@SpringBootApplication 
 public class ZbApplication {
 
 	public static void main(String[] args) {
